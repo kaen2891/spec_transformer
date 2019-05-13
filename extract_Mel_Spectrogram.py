665c83e4da06b31f4298e49e5f925258c8c89070
kaen2891 <kaen2891@gmail.com>
2019-05-13 14:15:52 +0900

    extract_mel_modified

--- extract_Mel_Spectrogram.py
+++ extract_Mel_Spectrogram.py
@@ -17,9 +17,7 @@ def Mel_S(wav_file, fs):
     print("wav length is ", len(y)/sr)
     S = librosa.feature.melspectrogram(y=y, n_mels=40, n_fft=input_nfft, hop_length=input_stride)
     '''
-    print("Wav length: {}, Mel_S shape:{}".format(len(y)/sr,np.shape(S)))
-
-
+    # plot figure
     plt.figure(figsize=(10, 4))
     librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
     plt.colorbar(format='%+2.0f dB')
@@ -30,74 +28,3 @@ def Mel_S(wav_file, fs):
     '''
 
     return S
-# man_original_data = '/mnt/junewoo/workspace/transform/test_wav/man_wav1.wav'
-# mel_spec = Mel_S(man_original_data)
-
-def jun_Mel_S(wav_file):
-    # mel-spectrogram
-    y, sr = librosa.load(wav_file, sr=16000)
-
-    # wav_length = len(y)/sr
-    input_nfft = int(round(sr*frame_length))
-    input_stride = int(round(sr*frame_stride))
-
-    S = librosa.feature.melspectrogram(y=y, n_mels=40, n_fft=input_nfft, hop_length=input_stride)
-    '''
-    print("Wav length: {}, Mel_S shape:{}".format(len(y)/sr,np.shape(S)))
-
-
-    plt.figure(figsize=(10, 4))
-    librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
-    plt.colorbar(format='%+2.0f dB')
-    plt.title('Mel-Spectrogram')
-    plt.tight_layout()
-    plt.savefig('Mel-Spectrogram example.png')
-    plt.show()
-    '''
-
-    return S
-
-
-
-'''
-#for test
-filename = '/data/dataset/IEMOCAP_All/IEMOCAP_full_release/Session3/sentences/wav/Ses03M_impro05a/Ses03M_impro05a_F020.wav'
-
-# mel-spectrogram
-(_, file_id) = os.path.split(filename)
-y, sr = librosa.load(filename, sr=16000)
-time = np.linspace(0, len(y)/sr, len(y)) # time axis
-fig, ax1 = plt.subplots()
-ax1.plot(time, y, color = 'b', label='speech waveform')
-ax1.set_xlabel("TIME [s]")
-plt.title(file_id)
-#plt.plot(time, y)
-plt.savefig(file_id+'.png')
-
-plt.show()
-
-#print("length y is : ", len(y))
-wav_length = len(y)/sr
-#print("wav file length : ", wav_length)
-
-input_nfft = int(round(sr*frame_length))
-#print("input nfft type: ",type(input_nfft))
-#print("nfft is : ", input_nfft)
-input_stride = int(round(sr*frame_stride))
-#print("stride is :", input_stride)
-#print("input hop_length type: ",type(input_stride))
-
-S = librosa.feature.melspectrogram(y=y, n_mels=40, n_fft=input_nfft, hop_length=input_stride)
-#print("S: ", S)
-#print("mels shape", np.shape(S))
-
-plt.figure(figsize=(10, 4))
-librosa.display.specshow(librosa.power_to_db(S, ref=np.max), y_axis='mel', sr=sr, hop_length=input_stride, x_axis='time')
-plt.colorbar(format='%+2.0f dB')
-plt.title(file_id+' Mel-Spectrogram')
-plt.tight_layout()
-plt.savefig(file_id+'Mel-Spectrogram.png')
-plt.show()
-'''
-
-
